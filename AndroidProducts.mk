e6afad920b5612fe54b3cf346c341e57c0a369be
Mikazuuu07 <irfn4566@gmail.com>
2022-02-10 01:04:42 +0000

    lava: xdroidify
    Signed-off-by: Mikazuuu07 <irfn4566@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The xdroidOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_lava.mk
+   $(LOCAL_DIR)/xdroid_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lava-user \
-    lineage_lava-userdebug \
-    lineage_lava-eng
+    xdroid_lava-user \
+    xdroid_lava-userdebug \
+    xdroid_lava-eng
